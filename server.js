85f2c73f61ea3b8b64a95cf878b5016e541e7bd2
Charles Shelton <vbsmbb@gmail.com>
2022-02-01 07:46:26 -0600

    Added an api with no variable to get current time. Updated the API with time to accept any time mode including time in milliseconds sine 1/1/1970.

--- server.js
+++ server.js
@@ -18,31 +18,58 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// The default dateTime API endpoint
+app.get('/api', (req,res) => {
+  let dUTC = new Date().toUTCString();
+  let msec = Date.parse(dUTC);
+  res.json({ unix: msec, utc: dUTC });
+});
+
 // The dateTime API endpoint
 app.get("/api/:date", function (req, res) {
   // Add the statements to get to point C!!!
-  const dateStr = req.params.date;
+  let dateStr = req.params.date;
+  dateStr.replace(/['"]+/g,'');
   console.log('Date string:',dateStr);
-  const dtRE =  /^\d{1,4}-\d{1,2}-\d{1,2}$/;
+  const dtYfRE =  /^\d{2,4}-\d{1,2}-\d{1,2}$/;
+  console.log('dtYfRE:',dtYfRE.test(dateStr));
+  const dtYlRE = /^\d{1,2}-\d{1,2}-\d{2,4}$/;
+  console.log('dtYlRE:',dtYlRE.test(dateStr));
+  const dtMDY = /^\w{3,} \d{1,2}, \d{2,4}$/;
+  console.log('dtMDY:',dtMDY.test(dateStr));
   const msRE = /^\d{1,13}$/;
+  console.log('msRE:',msRE.test(dateStr));
+  let dtFnd = false;
+  let dUTC = '';
+  let msec;
 
-  if ( dtRE.test(dateStr) ) {
-    let dUTC = new Date(dateStr).toUTCString();
-    let msec = Date.parse(dateStr);
-    res.json({ unix: msec, utc: dUTC }); 
-  } else if ( msRE.test(dateStr) ) {
-    let msec = parseInt(dateStr);
-    let dUTC = new Date(msec).toUTCString();
-    res.json({ unix: msec, utc: dUTC });
-  } else {
-    console.error("Incorrect date string format");
+  switch (true) {
+    case dtYfRE.test(dateStr) ||
+         dtYlRE.test(dateStr) ||
+         dtMDY.test(dateStr):
+      dUTC = new Date(dateStr).toUTCString();
+      msec = Date.parse(dateStr);
+      dtFnd = true;
+      break;
+    case msRE.test(dateStr):
+      msec = parseInt(dateStr);
+      dUTC = new Date(msec).toUTCString();
+      dtFnd = true;
+      break;
+    case (dateStr === null):
+      dUTC = new Date().toUTCString();
+      msec = Date.parse(dUTC);
+      dtFnd = true;
+      break;
+    default:
+      res.json({error: "Invalid Date"});
   }
+  if (dtFnd) { res.json({ unix: msec, utc: dUTC }); }
 });
 
 // listen for requests :)
